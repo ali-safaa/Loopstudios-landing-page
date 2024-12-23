62a3da917aaad339fcd0c192ff7e170eb3129cc5
ali safaa <alisafaaa33@gmail.com>
2024-12-23 22:32:31 +0300

    done

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const container = document.querySelector("[data-container]");
+const close_icon = document.querySelector("[data-close-icon]");
+const hamburger_icon = document.querySelector("[data-hamburger-icon]");
+
+hamburger_icon.addEventListener("click", () => {
+     container.style.display = "block";
+});
+
+close_icon.addEventListener("click", () => {
+     container.style.display = "none";
+});
+
 window.addEventListener("scroll", function () {
      const header = document.querySelector("[data-header]");
      if (window.scrollY >= 600) {
